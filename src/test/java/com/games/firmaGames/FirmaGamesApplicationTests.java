7cbba9aa34d25d22c8dfa35c06a1b31e070d7d97
RiqueMendes <97553942+RiqueMendes@users.noreply.github.com>
2022-05-02 10:42:31 -0300

    create project

---
+++ src/test/java/com/games/firmaGames/FirmaGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.firmaGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmaGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
