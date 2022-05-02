384a0369471546ce8c62a310adbf82357144c62d
Saturno <samuelmathias033@gmail.com>
2022-05-02 19:24:46 -0300

    @samuelsaturn [Task: att repository]

--- src/main/java/com/games/firmaGames/Repository/CategoryRepository.java
+++ src/main/java/com/games/firmaGames/Repository/CategoryRepository.java
@@ -3,9 +3,11 @@ package com.games.firmaGames.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.games.firmaGames.Model.Category;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 	
 	public List<Category> findAllByGenderContainingIgnoreCase(String gender);
--- src/main/java/com/games/firmaGames/Repository/ProductRepository.java
+++ src/main/java/com/games/firmaGames/Repository/ProductRepository.java
@@ -3,9 +3,11 @@ package com.games.firmaGames.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.games.firmaGames.Model.Products;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Products, Long> {
 	
 	public List<Products> findAllByDescriptionContainingIgnoreCase(String description);
