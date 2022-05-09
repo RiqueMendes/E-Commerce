8a0e07266af599c8e7970f54208b526e5704c26d
RiqueMendes <97553942+RiqueMendes@users.noreply.github.com>
2022-05-09 10:40:22 -0300

    @Riquemendes [task: Swagger]

---
+++ src/main/java/com/games/firmaGames/configuration/SwaggerConfig.java
@@ -0,0 +1,56 @@
+
+package com.games.firmaGames.configuration;
+
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springBlogPessoalOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Projeto Blog Pessoal")
+                        .description("Projeto Blog Pessoal - Generation Brasil")
+                        .version("v0.0.1")
+                        .license(new License()
+                                .name("Generation Brazil")
+                                .url("https://brazil.generation.org/"))
+                        .contact(new Contact()
+                                .name("Repositório GitHub")
+                                .url("https://github.com/samuelsaturn/BlogPessoal")
+                                .email("samuelmathias033@gmail.com")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Meu Github")
+                        .url("https://github.com/samuelsaturn"));
+    }
+
+    private ApiResponse createApiResponse(String message) {
+        return new ApiResponse().description(message);
+    }
+
+    @Bean
+    public OpenApiCustomiser customerGlobalResponseStatus() {
+        return openApi -> {
+            openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+                ApiResponses api = operation.getResponses();
+
+                api.addApiResponse("200", createApiResponse("Sucess!"));
+                api.addApiResponse("201", createApiResponse("Created!"));
+                api.addApiResponse("400", createApiResponse("Request error!"));
+                api.addApiResponse("401", createApiResponse("Not authorized!"));
+                api.addApiResponse("500", createApiResponse("Internal server Error!"));
+            }));
+        };
+    }
+}
